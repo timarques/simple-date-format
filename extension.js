020d9f90a193f1984a7e71fc7f95d175c467bc67
Tiago Marques <tyimarques@gmail.com>
2019-03-05 00:41:30 +0000

    First change of initial code

--- extension.js
+++ extension.js
@@ -4,55 +4,30 @@ const main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const Convenience = Me.imports.convenience;
 
 let originalClockDisplay;
 let formatClockDisplay;
-let settings;
 let timeoutID = 0;
 
-/**
- * Initialising function which will be invoked at most once directly after your source JS file is loaded.
- */
 function init() {
   originalClockDisplay = main.panel.statusArea.dateMenu._clockDisplay;
   formatClockDisplay = new St.Label({
     y_align: Clutter.ActorAlign.CENTER,
   });
-  settings = Convenience.getSettings();
-
-  // FIXME: Set settings first time to make it visible in dconf Editor
-  if (!settings.get_string('format')) {
-    settings.set_string('format', '%Y.%m.%d %H:%M');
-  }
 }
 
-/**
- * Enable, called when extension is enabled or when screen is unlocked.
- */
 function enable() {
   originalClockDisplay.hide();
   originalClockDisplay.get_parent().insert_child_below(formatClockDisplay, originalClockDisplay);
-  timeoutID = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, tick);
+  timeoutID = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, function(){
+    formatClockDisplay.set_text(new Date().toLocaleFormat('%Y.%m.%d %H:%M'));
+    return true;
+  });
 }
 
-/**
- * Disable, called when extension is disabled or when screen is locked.
- */
 function disable() {
   GLib.Source.remove(timeoutID);
   timeoutID = 0;
   originalClockDisplay.get_parent().remove_child(formatClockDisplay);
   originalClockDisplay.show();
-}
-
-/**
- * It runs every time we need to update clock.
- * @return {boolean} Always returns true to loop.
- */
-function tick() {
-  const format = settings.get_string('format');
-  formatClockDisplay.set_text(new Date().toLocaleFormat(format));
-
-  return true;
 }
